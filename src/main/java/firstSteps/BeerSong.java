5343cc4475d4211a3578b0e2a92f4898f0f83a92
germanmalykh <hcM5@rrh4>
2022-09-20 10:01:21 +0300

    fix BeerSong

--- src/main/java/firstSteps/BeerSong.java
+++ src/main/java/firstSteps/BeerSong.java
@@ -6,16 +6,18 @@ public class BeerSong {
         String word = "бутылок (бутылки)";
 
         while (beerNum > 0) {
+
             if (beerNum == 1) {
                 word = "бутылка";
             }
-
-            System.out.println(beerNum + " " + word + " пива на стене");
+            System.out.println("\n" + beerNum + " " + word + " пива на стене");
             System.out.println(beerNum + " " + word + " пива.");
             System.out.println("Возьми одну.");
             System.out.println("Пусти по кругу");
             beerNum = beerNum - 1;
-            if (beerNum > 0) {
+            if (beerNum > 1) {
+                System.out.println(beerNum + " " + word + " пива на стене");
+            } else if (beerNum == 1) {
                 System.out.println(beerNum + " " + word + " пива на стене");
             } else {
                 System.out.println("Нет бутылок пива на стене");
